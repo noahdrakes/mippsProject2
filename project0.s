d73354e831450566efb112f3e86bf63800e8bc42
noahdrakes <noah.drakes@bison.howard.edu>
2022-03-02 16:03:58 -0500

    establishing bison id and divsor variables, storing modulus

--- project0.s
+++ project0.s
@@ -3,4 +3,10 @@ out_string:     .asciiz "\nHello, 02927184\n"   # putting string in memory
 .text               #where mipps code is used
 main:
     
-    li $t0, 0  #clearing the $t0 register   
+    li $t0, 0  #clearing the $t0 register
+    li $t8, 02927184 #loading bison id
+    li $t9, 11 #divisor
+
+    div $t8, $t9 #calulcation
+    mfhi $t0 # n value 
+loop:   
