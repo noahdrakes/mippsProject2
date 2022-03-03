c22a40637c9983d5663eb1522525c449a72341fd
noahdrakes <noah.drakes@bison.howard.edu>
2022-03-02 23:15:47 -0500

    remove extra space

--- project0.s
+++ project0.s
@@ -1,5 +1,5 @@
 .data           #data secion - location of all data in memory
-out_string:     .asciiz "\nHello, 02927184\n"   # putting string in memory   
+out_string:     .asciiz "Hello, 02927184\n"   # putting string in memory   
 .text               #where mipps code is used
 main:
     
