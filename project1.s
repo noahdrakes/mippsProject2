36ea1580a8d094720481698e3421c2ed4fa1ec76
noahdrakes <noah.drakes@bison.howard.edu>
2022-03-26 22:25:25 -0400

    calculated M and N

--- project1.s
+++ project1.s
@@ -9,3 +9,10 @@
 
 
 main:
+
+    li $t8, 02927184 #loading bison id
+    li $t9, 11 #divisor
+
+    div $t8, $t9 #calulcation
+    mfhi $t0 # n value 
+    addi $s0, $t0, 26 #N is in $s0 add 26 to modulus
