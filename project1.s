dee6f224a98975e72d2829443d6b377bc270f216
noahdrakes <noah.drakes@bison.howard.edu>
2022-03-26 22:22:21 -0400

    allocated space for newLine character and space to store string input

---
+++ project1.s
@@ -0,0 +1,11 @@
+.data   #data secion - location of all data in memory
+    userInput: .space 12
+    newLineCharacter: .asciiz "\n"
+
+
+.text               #where mipps code is used
+
+
+
+
+main:
