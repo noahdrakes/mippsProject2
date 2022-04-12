83f4d4401826c558a758a2f0d38c9023746f4aff
noahdrakes <noah.drakes@bison.howard.edu>
2022-04-12 18:18:07 -0400

    code cleanup, final iteration

--- project2.s
+++ project2.s
@@ -38,7 +38,7 @@ main:
     li $t6, 0
 
 
-    #                       **REMOVING BEGINNING SPACES AND TABS **
+    #                       **REMOVING BEGINNING SPACES AND TABS FROM BEGINNING **
 
 
     li $t5, 0 #increment for removeSpaces and Tabs
@@ -63,18 +63,13 @@ main:
         addi $t7, $t7, 1 #increment index for array of user input characters
         j removeSpacesAndTabs
 
-
+    #                       **STORE UP TO THE FIRST FOUR LEGITIMATE VALUES**
     
-    #                       **STORE THE FIRST FOUR LEGITIMATE VALUES**
-    
-
-
-    #using register $t3 for array
 
     
     storeRealValues:  
         li $t4, 0                   #increment for store real values
-        li $t3, 3                   #load address to store array of 4 bytes for the 4 real characters
+        #li $t3, 0                   #load address to store array of 4 bytes for the 4 real characters
 
         loopStoreRealValues:  
             beq $t4, 4, checkRemainingTrailingCharacters        #check if increment is less than 4
@@ -88,22 +83,12 @@ main:
             beq $t6, 10, check4CharactersArray   #if character is new line character character -> end of string, determine if its valid
             beq $t6, 0, check4CharactersArray   #if character is null terminating character -> end of string, determine if its valid
 
-            sb $t6, array4characters($t3)                       #store valid characters in new array                   
-
-            
-
-            
-
-            
+            sb $t6, array4characters($t4)                       #store valid characters in new array                   
+  
             
             addi $t4, $t4, 1        #increment loop
             addi $t7, $t7, 1        #increment index for array of user input characters
-            addi $t3, $t3, -1        #increment index for character array
-            addi $t5, $t5, 1        #increment original character loop to skip the valid character and check for invalid characters in the check trailing remain characters function
             lb $t6, 0($t7)          #get next character from four bit array
-
-
-            
              
             j loopStoreRealValues
 
@@ -126,21 +111,14 @@ main:
 
             j checkRemainingTrailingCharacters
 
-
-
 invalidInput:
     li $v0, 4       #selecting print function for syscall
     la $a0, invalidInputString  #selecting address of string
     syscall
     j exitProgram
 
-
-
-
 check4CharactersArray:
     li $t5, 0          #index of loop
-
-    li $t7, 3           #index of 4character array
 
     li $t2, 0           #reg for storing the correct decimal value for characters
 
@@ -155,19 +133,14 @@ check4CharactersArray:
 
     loop2:
         beq $t5, $t4, printAnswer     #once loop ends print answer
-        lb $t6, array4characters($t7)       #get character from (potentially) valid character away
+        lb $t6, array4characters($t5)       #get character from (potentially) valid character array
 
         beq $t6, 11, invalidInput   #if character is line tab -> jump to invalid Input
         beq $t6, 9, invalidInput    #if character is char tab -> jump to invalid input
         beq $t6, 32, invalidInput   #if character is space    -> jump to invalid input
 
-        li $t1, 1           #reg for carrying exponent
 
-
-
-
-
-
+        #logic for converting ASCII values to decimal 
         ble $t6, 57, decimal
         blt $t6, $s3, capitalLetters
         blt $t6, $t3, lowercaseLetters
@@ -187,10 +160,6 @@ check4CharactersArray:
             blt $t6, 97, invalidInput
             addi $t2, $t6, -87
             j exponentLoop
-
-
-
-        #add $t0, $t0, $t5
 
         exponentLoop:
 
@@ -218,15 +187,8 @@ check4CharactersArray:
             beq $s8, 2, exponent2
             beq $s8, 3, exponent3
 
-
-
-
-
             exponent0:
-                mult $t1, $t2
-                mflo $t2
                 j sum
-
             exponent1:
                 mult $s1, $t2
                 mflo $t2
@@ -239,8 +201,6 @@ check4CharactersArray:
                 mflo $t2
                 j sum
             exponent3:
-                #beq $t4, 1, exponent0
-
                 li $t8, 0
                 li $t9, 0
                 mult $s1, $s1
@@ -251,13 +211,11 @@ check4CharactersArray:
                 mflo $t2
                 j sum
 
-
-
         sum:
-            add $t0, $t0, $t2       #sum everything
+            add $t0, $t0, $t2       #   sum everything
 
-            addi $t5, $t5, 1
-            addi $t7, $t7, -1
+            addi $t5, $t5, 1        #   increment index
+            addi $t7, $t7, 1       #   decrement array index (storing backwards)
             j loop2
 
 printAnswer:
